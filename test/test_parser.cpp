4095ab71272d8f036f6799c3fa3bd7f961f59879
Yoshua Wakeham <yoshwakeham@gmail.com>
2015-08-03 16:28:14 +1000

    Create tests directory and initial Catch setup

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -1,10 +1,10 @@
-#include "params.hpp"
+#include "parser.hpp"
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
 
 int main(int argc, char *argv[]) {
-    po::variables_map vm = params::parse_options(argc, argv);
+    po::variables_map vm = parser::parse_options(argc, argv);
 
     return 0;
 }
