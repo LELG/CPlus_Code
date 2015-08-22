88999ffc56ba5c659cf4d40128195116595ecd2b
Yoshua Wakeham <yoshua.wakeham@petermac.org>
2015-08-22 14:04:07 +1000

    Write first test for parser. Just trying to get familiar with writing tests for Catch.

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -1,14 +1,20 @@
-#include <iostream>
+#include "catch.hpp"
+#include <stdexcept>
 #include "parser.hpp"
-#include <boost/program_options.hpp>
 
-namespace po = boost::program_options;
+SCENARIO( "parser checks for mutually exclusive options", "[parser]" )
+{
+    GIVEN( "a call to parser::parse_options" )
+    {
+        WHEN( "both --init_size and --init_diversity are specified" )
+        {
+            char *argv[] = {"--init_size", "25",
+                            "--init_diversity", "foo.csv", "bar.csv"};
 
-int main(int argc, char *argv[]) {
-    po::variables_map vm = parser::parse_options(argc, argv);
-
-    using parser::operator<<;
-    std::cout << vm << std::endl;
-
-    return 0;
+            THEN( "a logic error is thrown" )
+            {
+                REQUIRE_THROWS_AS( parser::parse_options(5, argv), std::logic_error );
+            }
+        }
+    }
 }
