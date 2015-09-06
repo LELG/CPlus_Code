640a66528b681714bdec43f00d0ae62e199edd72
Yoshua Wakeham <yoshua.wakeham@petermac.org>
2015-09-06 11:16:06 +1000

    Move Random class to separate module.

---
+++ Random.cpp
@@ -0,0 +1,81 @@
+#include "Random.hpp"
+#include <random>
+
+unsigned int Random::Recovery_After_Replication()
+{
+    return uniform_int_distribution<unsigned int>{12, 15}(eng);
+}
+
+unsigned int Random::G1()
+{
+    return uniform_int_distribution<unsigned int>{12, 15}(eng);
+}
+
+unsigned int Random::S()
+{
+    return uniform_int_distribution<unsigned int>{5, 10}(eng);
+}
+
+unsigned int Random::G2()
+{
+    return uniform_int_distribution<unsigned int>{3, 5}(eng);
+}
+
+unsigned int Random::Poisson()
+{
+    return poisson_distribution<unsigned int>{1}(eng);
+}
+
+double Random::Z()
+{
+    return normal_distribution<double>{0.0,1.0}(eng);
+}
+
+double Random::Binomial_dying(unsigned int Clone_Size, double Death_Rate)
+{
+    return binomial_distribution<unsigned int>{Clone_Size, Death_Rate}(eng);
+}
+
+double Random::Binomial_newborn(unsigned int Clone_Size, double Adjusted_Proliferation_Rate)
+{
+    return binomial_distribution<unsigned int>{Clone_Size, Adjusted_Proliferation_Rate }(eng);
+}
+
+unsigned int Random::Binomial_Mutants(unsigned int NewBorn_Size, double Mutation_Rate)
+{
+    return binomial_distribution<unsigned int>{NewBorn_Size, Mutation_Rate}(eng);
+}
+
+double Random::Uniform_Mutation_Rate(double mu_rate)
+{
+    return uniform_real_distribution<double>{mu_rate/2.0, mu_rate*2.0}(eng);
+}
+double Random::Update_Proliferation_Rate(double Parent_Proliferation_Rate)
+{
+    double U = normal_distribution<double>{Parent_Proliferation_Rate, 0.001 }(eng);
+    if(U > 1.0)
+    {
+        U = 1.0;
+    }
+    if (U < 0.0)
+    {
+        U = 0.0;
+    }
+
+    return U;
+}
+
+double Random::Uniform_Mutation_Rate_2(double Parent_mu_rate)
+{
+
+    double U = normal_distribution<double>{Parent_mu_rate, mut_rate }(eng);
+    if(U > 1.0)
+    {
+        U = 1.0;
+    }
+    if (U < 0.0)
+    {
+        U = 0.0;
+    }
+    return U;
+}
--- V1_Main_Tumour_Evoulution.cpp
+++ V1_Main_Tumour_Evoulution.cpp
@@ -56,6 +56,8 @@ namespace fs = boost::filesystem;
 #include "parser.hpp"
 namespace po = boost::program_options;
 
+#include "Random.hpp"
+
 
 /******************************* Constant Varaible Definitions *******************************/
 
@@ -404,115 +406,14 @@ namespace core {
 		return unique_ptr<Clonal_Expansion>( new Clonal_Expansion(params) );
 	}// end function
 
-	class Random 
-	{
-		public:
-
-    	Random() = default;
-    	Random(std::mt19937::result_type seed) : eng(seed) {}
-    	unsigned int Recovery_After_Replication();
-    	unsigned int G1();
-    	unsigned int S();
-    	unsigned int G2();
-    	unsigned int Poisson();
-    	double Z();
-    	double Binomial_dying(unsigned int Clone_Size, double Death_Rate);
-    	double Binomial_newborn(unsigned int Clone_Size, double Adjusted_Proliferation_Rate);
-    	unsigned int Binomial_Mutants(unsigned int NewBorn_Size, double Mutation_Rate);
-    	//unsigned int* Mitosis_Multinomial(unsigned int Clone_Size);
-    	//unsigned int Uniform_Mutation_Division_Memebers(unsigned int Clone_Size);
-    	double Uniform_Mutation_Rate(double mu_rate);
-    	double Update_Proliferation_Rate(double Proliferation_Rate);
-    	double Uniform_Mutation_Rate_2(double Parent_mu_rate);
 
 
-		private:        
-    	std::mt19937 eng{std::random_device{}()};
-    	
-
-  
- 		
-	};
 
 
-	unsigned int Random::Recovery_After_Replication()
-	{
-    	return uniform_int_distribution<unsigned int>{12, 15}(eng);
-	}
 
-	unsigned int Random::G1()
-	{
-		return uniform_int_distribution<unsigned int>{12, 15}(eng);
-	}
 
-	unsigned int Random::S()
-	{
-		return uniform_int_distribution<unsigned int>{5, 10}(eng);
-	}
 
-	unsigned int Random::G2()
-	{
-		return uniform_int_distribution<unsigned int>{3, 5}(eng);
-	}
 
-	unsigned int Random::Poisson()
-	{
-		return poisson_distribution<unsigned int>{1}(eng);
-	}
-
-	double Random::Z()
-	{
-		return normal_distribution<double>{0.0,1.0}(eng);
-	}
-
-	double Random::Binomial_dying(unsigned int Clone_Size, double Death_Rate)
-	{
-		return binomial_distribution<unsigned int>{Clone_Size, Death_Rate}(eng);
-	}
-	
-	double Random::Binomial_newborn(unsigned int Clone_Size, double Adjusted_Proliferation_Rate)
-	{
-		return binomial_distribution<unsigned int>{Clone_Size, Adjusted_Proliferation_Rate }(eng);
-	}
-
-	unsigned int Random::Binomial_Mutants(unsigned int NewBorn_Size, double Mutation_Rate)
-	{
-		return binomial_distribution<unsigned int>{NewBorn_Size, Mutation_Rate}(eng);
-	}
-
-	double Random::Uniform_Mutation_Rate(double mu_rate)
-	{
-		return uniform_real_distribution<double>{mu_rate/2.0, mu_rate*2.0}(eng);
-	}
-	double Random::Update_Proliferation_Rate(double Parent_Proliferation_Rate)
-	{
-		double U = normal_distribution<double>{Parent_Proliferation_Rate, 0.001 }(eng);
-		if(U > 1.0)
-		{
-			U = 1.0;
-		}
-		if (U < 0.0)
-		{
-			U = 0.0;
-		}
-		
-		return U;
-	}
-
-	double Random::Uniform_Mutation_Rate_2(double Parent_mu_rate)
-	{
-
-		double U = normal_distribution<double>{Parent_mu_rate, mut_rate }(eng);
-		if(U > 1.0)
-		{
-			U = 1.0;
-		}
-		if (U < 0.0)
-		{
-			U = 0.0;
-		}
-		return U;
-	}
 
 	Random r;
 
