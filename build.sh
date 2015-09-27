e65e4d9cce2639ca4726369035c475d7ac523255
Yoshua Wakeham <yoshua.wakeham@petermac.org>
2015-09-27 10:26:40 +1000

    Add option to compile treatment code

--- build.sh
+++ build.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
-echo "==> Starting simulation build"
+script_parameters="[--treatment]"
+
+function usage-exit () {
+  echo "Usage: ./`basename $0` $script_parameters"
+  echo >&2 $@
+  exit $E_WRONG_ARGS
+}
+
+if [ $# -ne 0 -a $# -ne 1 ]
+then
+  usage-exit "Incorrect number of parameters provided"
+elif [ $# -eq 0 ]
+then
+  treatment=false
+elif [ "$1" = --treatment ]
+then
+  treatment=true
+else
+  usage-exit "Unknown option '"$1"'"
+fi
+
+if [ "$treatment" = true ]
+then
+  echo "==> Starting simulation build (version with TREATMENT)"
+else
+  echo "==> Starting simulation build"
+fi
 
 mkdir build 2> /dev/null
 
@@ -30,7 +56,13 @@ fi
 
 echo "==> Running CMake --build ..."
 
-cmake --build build >>build/build.log 2>&1
+printf "\nbuilding CMake target\n\n" >> build/build.log
+if [ "$treatment" = true ]
+then
+  ( cd build && make tumourSimTreatment >> build.log 2>&1 )
+else
+  ( cd build && make tumourSim >> build.log 2>&1 )
+fi
 
 if [ $? -ne 0 ]; then
   echo "ERR: CMake build failed. See build/build.log for details." >&2
